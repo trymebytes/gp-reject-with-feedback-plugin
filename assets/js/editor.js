269eb1752d326b1d183686be5239c0f402847cce
Tosin Oguntuyi <tosin.tuyi@gmail.com>
2021-09-05 22:52:13 +0100

    Add new values to ajax payload

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -3,16 +3,21 @@
 	$gp.editor.hooks.set_status_rejected = function() {
 		button = $( this );
 		var gp_rejection_feedback = prompt('Enter reason for rejection');
-		//extract locale from page url
-		path = location.pathname.substr(1).slice(0, -1).split('/');
-		locale = path[path.length - 2];
+		//extract details from page url
+		let path = location.pathname.substr(1).slice(0, -1).split('/');
+		let project_path = path[path.length - 3]
+		let locale_slug = path[path.length - 2];
+		let translation_set_slug = path[path.length - 1];
+		
 		
 		payload = {};
 		payload.rejection_feedback = gp_rejection_feedback;
-		payload.locale = locale;
+		payload.project_path = project_path;
+		payload.locale_slug = locale_slug;
+		payload.translation_set_slug = translation_set_slug;
 		payload.original_id = $gp.editor.current.original_id;
 		payload.translation_id = $gp.editor.current.translation_id;
-		
+				
 		jQuery( document ).ready( function () {
 			const data = {
 				action: 'reject_with_feedback',
@@ -26,6 +31,7 @@
 				success: function( data ) {
 					button.prop( 'disabled', false );
 					$gp.notices.success( 'Translation Rejected!' );
+					$gp.editor.replace_current( data );
 					$gp.editor.next();
 				},
 				error: function( xhr, msg ) {
